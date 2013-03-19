d4163c3820bef3c6b5fa0623bd19de8165d016c4
Julien Viet <julien@julienviet.com>
2013-03-19 17:17:55 +0100

    Update to 1.2.0-cr9 and make the command per user work

--- src/main/java/org/crsh/web/LifeCycle.java
+++ src/main/java/org/crsh/web/LifeCycle.java
@@ -1,17 +1,27 @@
 package org.crsh.web;
 
+import org.crsh.command.GroovyScript;
+import org.crsh.command.GroovyScriptCommand;
+import org.crsh.command.ShellCommand;
+import org.crsh.plugin.CRaSHPlugin;
 import org.crsh.plugin.PluginContext;
+import org.crsh.plugin.PluginDiscovery;
+import org.crsh.plugin.ResourceKind;
+import org.crsh.plugin.SimplePluginDiscovery;
 import org.crsh.plugin.WebPluginLifeCycle;
 import org.crsh.shell.Shell;
 import org.crsh.shell.ShellFactory;
-import org.crsh.shell.impl.command.CRaSHShellFactory;
+import org.crsh.shell.impl.command.AbstractClassManager;
+import org.crsh.shell.impl.command.CRaSH;
+import org.crsh.shell.impl.command.ClassManager;
 import org.crsh.util.Safe;
+import org.crsh.util.TimestampedObject;
 import org.crsh.vfs.FS;
 import org.crsh.vfs.Path;
+import org.crsh.vfs.Resource;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
@@ -20,8 +30,9 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.security.Principal;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicReference;
 
 /** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
 @WebListener
@@ -36,7 +47,7 @@ public class LifeCycle extends WebPluginLifeCycle implements HttpSessionListener
   private static final ConcurrentHashMap<String, LifeCycle> registry = new ConcurrentHashMap<String, LifeCycle>();
 
 	/** . */
-	private CRaSHShellFactory crash;
+	private ShellFactory crash;
 
   /** . */
   private final SimpleFS commands = new SimpleFS();
@@ -49,10 +60,9 @@ public class LifeCycle extends WebPluginLifeCycle implements HttpSessionListener
     commands.current.remove();
   }
 
-  public void sessionCreated(HttpSessionEvent se)
-	{
+  public void sessionCreated(HttpSessionEvent se) {
 		HttpSession session = se.getSession();
-		Shell shell = crash.create(null, false);
+		Shell shell = crash.create(null);
 		session.setAttribute("crash", new SerializableTransient<Shell>(shell));
 	}
 
@@ -74,11 +84,61 @@ public class LifeCycle extends WebPluginLifeCycle implements HttpSessionListener
     PluginContext context = getPluginContext(sce.getServletContext());
 
     //
-    crash = (CRaSHShellFactory)context.getPlugin(ShellFactory.class);
+    crash = context.getPlugin(ShellFactory.class);
 
     //
     registry.put(sce.getServletContext().getContextPath(), this);
 	}
+
+  @Override
+  protected PluginDiscovery createDiscovery(ServletContext context, ClassLoader classLoader) {
+    class Factory extends CRaSHPlugin<ShellFactory> implements ShellFactory {
+      private CRaSH crash;
+      @Override
+      public void init() {
+        final PluginContext context = getContext();
+        crash = new CRaSH(
+          context,
+          new AbstractClassManager<ShellCommand>(context, ShellCommand.class, GroovyScriptCommand.class) {
+            @Override
+            protected TimestampedObject<Class<? extends ShellCommand>> loadClass(String name) {
+              HttpSession session = commands.current.get().getSession(false);
+              if (session != null) {
+                HashMap<String, TimestampedObject<Class<? extends ShellCommand>>> classes = (HashMap<String, TimestampedObject<Class<? extends ShellCommand>>>)session.getAttribute("classes");
+                if (classes != null) {
+                  return classes.get(name);
+                }
+              }
+              return null;
+            }
+            @Override
+            protected void saveClass(String name, TimestampedObject<Class<? extends ShellCommand>> clazz) {
+              HttpSession session = commands.current.get().getSession();
+              HashMap<String, TimestampedObject<Class<? extends ShellCommand>>> classes = (HashMap<String, TimestampedObject<Class<? extends ShellCommand>>>)session.getAttribute("classes");
+              if (classes == null) {
+                session.setAttribute("classes", classes = new HashMap<String, TimestampedObject<Class<? extends ShellCommand>>>());
+              }
+              classes.put(name, clazz);
+            }
+            @Override
+            protected Resource getResource(String name) {
+              return context.loadResource(name, ResourceKind.COMMAND);
+            }
+          },
+          new ClassManager<GroovyScript>(context, ResourceKind.LIFECYCLE, GroovyScript.class, GroovyScript.class));
+      }
+      @Override
+      public ShellFactory getImplementation() {
+        return this;
+      }
+      public Shell create(Principal principal) {
+        return crash.createSession(principal);
+      }
+    }
+    SimplePluginDiscovery discovery = new SimplePluginDiscovery();
+    discovery.add(new Factory());
+    return discovery;
+  }
 
   @Override
   protected FS createCommandFS(ServletContext context) {
