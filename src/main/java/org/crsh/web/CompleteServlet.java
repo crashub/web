47ee7f0aeece391cbfd29d13fa0533495c06a0a6
Julien Viet <julien@julienviet.com>
2013-03-22 14:55:03 +0100

    Avoid to complete with no prefix

--- src/main/java/org/crsh/web/CompleteServlet.java
+++ src/main/java/org/crsh/web/CompleteServlet.java
@@ -33,10 +33,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
 @WebServlet(urlPatterns = "/complete")
 public class CompleteServlet extends HttpServlet {
+
+  /** . */
+  private static final Logger log = Logger.getLogger(CompleteServlet.class.getSimpleName());
 
   /** . */
   static final Gson gson = new Gson();
@@ -47,40 +51,46 @@ public class CompleteServlet extends HttpServlet {
     Session session = lf.getSession();
     Shell shell = session.getShell();
     String prefix = req.getParameter("prefix");
-    CompletionMatch completion = shell.complete(prefix);
-    Completion completions = completion.getValue();
-    List<String> values = new ArrayList<String>();
-    if (completions.getSize() == 0) {
-      // Do nothing
-    }
-    else {
-      Delimiter delimiter = completion.getDelimiter();
-      StringBuilder sb = new StringBuilder();
-      if (completions.getSize() == 1) {
-        String value = completions.getValues().iterator().next();
-        delimiter.escape(value, sb);
-        if ((Boolean)completions.get(value)) {
-          sb.append(delimiter.getValue());
-        }
-        values.add(sb.toString());
+    if (prefix != null) {
+      log.info(req.getRemoteHost() + " completing " + prefix);
+      CompletionMatch completion = shell.complete(prefix);
+      Completion completions = completion.getValue();
+      List<String> values = new ArrayList<String>();
+      if (completions.getSize() == 0) {
+        // Do nothing
       }
       else {
-        String commonCompletion = Strings.findLongestCommonPrefix(completions.getValues());
-        if (commonCompletion.length() > 0) {
-          delimiter.escape(commonCompletion, sb);
+        Delimiter delimiter = completion.getDelimiter();
+        StringBuilder sb = new StringBuilder();
+        if (completions.getSize() == 1) {
+          String value = completions.getValues().iterator().next();
+          delimiter.escape(value, sb);
+          if ((Boolean)completions.get(value)) {
+            sb.append(delimiter.getValue());
+          }
           values.add(sb.toString());
         }
         else {
-          String completionPrefix = completions.getPrefix();
-          for (Map.Entry<String, Boolean> entry : completions) {
-            sb.append(completionPrefix);
-            delimiter.escape(entry.getKey(), sb);
+          String commonCompletion = Strings.findLongestCommonPrefix(completions.getValues());
+          if (commonCompletion.length() > 0) {
+            delimiter.escape(commonCompletion, sb);
             values.add(sb.toString());
-            sb.setLength(0);
+          }
+          else {
+            String completionPrefix = completions.getPrefix();
+            for (Map.Entry<String, Boolean> entry : completions) {
+              sb.append(completionPrefix);
+              delimiter.escape(entry.getKey(), sb);
+              values.add(sb.toString());
+              sb.setLength(0);
+            }
           }
         }
       }
+      resp.getWriter().write(gson.toJson(values));
+    } else {
+      log.info(req.getRemoteHost() + " no completion prefix for " + req.getRequestURL() + " " + req.getParameterMap());
+      resp.getWriter().write("[]");
     }
-    resp.getWriter().write(gson.toJson(values));
   }
 }
